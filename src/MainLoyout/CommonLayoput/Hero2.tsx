8c66dc8710f50975ef105eef0a81c55dd8475bbb
Tanver Mahidi <tanveermahidi@gmail.com>
2026-04-12 18:37:40 +0600

    Updated admission form and schema

--- src/MainLoyout/CommonLayoput/Hero2.tsx
+++ src/MainLoyout/CommonLayoput/Hero2.tsx
@@ -1,5 +1,5 @@
-"use client";
-import React, { useState, useEffect } from 'react';
+
+import  { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { Star, Users, ChevronRight, CheckCircle2, Bell } from 'lucide-react';
 import { Button } from "@/components/ui/button";
